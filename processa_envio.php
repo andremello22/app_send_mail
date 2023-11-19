6082bb1fd60b96602d0554ca1cba5e3eb47429ea
andremello22 <andremelloti@outlook.com>
2023-11-19 16:02:18 -0300

    v1

---
+++ processa_envio.php
@@ -0,0 +1,10 @@
+<?php 
+require "../../app_send_mail/bibliotecas/phpmailer/Exception.php";
+require "../../app_send_mail/bibliotecas/phpmailer/OAuth.php";
+require "../../app_send_mail/bibliotecas/phpmailer/PHPMailer.php";
+require "../../app_send_mail/bibliotecas/phpmailer/POP3.php";
+require "../../app_send_mail/bibliotecas/phpmailer/SMTP.php";
+
+require "../../app_send_mail/processa_envio.php"; 
+
+?>
